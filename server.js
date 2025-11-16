e1fbecfcb90753e65b8a9b30752eabf84032b7ac
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2025-11-16 17:22:53 +0200

    Listen localhost only

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app"); // Import the configured app
 const port = process.env.EXPRESS_PORT || 3000;
 
 // This is the ONLY place in your entire project where app.listen should be called.
-app.listen(port, () => {
+app.listen(port, "localhost", () => {
   console.log(`ELR-Converter API listening at port ${port}`);
   console.log(`API documentation available at /api-docs`);
 });
